e1b11f85b9148bcf83daed9586fb2f1b23a5529d
Ahmed Fasih <wuzzyview@gmail.com>
2020-08-02 23:30:34 -0400

    oops, need all JS files

---
+++ interfaces.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var GlossType;
+(function (GlossType) {
+    GlossType["literal"] = "literal";
+    GlossType["figurative"] = "figurative";
+    GlossType["explanation"] = "explanation";
+})(GlossType = exports.GlossType || (exports.GlossType = {}));
