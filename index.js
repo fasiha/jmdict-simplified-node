c194048f220000ef9da6d488f5d91b918a672457
Hocky Yudhiono <hocky.yudhiono@tiket.com>
2023-07-28 12:38:36 +0700

    Update typescript

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,29 +22,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getField = exports.getTags = exports.kanjiAnywhere = exports.kanjiBeginning = exports.readingAnywhere = exports.readingBeginning = exports.idsToWords = exports.setup = void 0;
 const fs_1 = require("fs");
-const leveldown_1 = __importDefault(require("leveldown"));
-const levelup_1 = __importDefault(require("levelup"));
-__export(require("./interfaces"));
+const classic_level_1 = require("classic-level");
+const level_read_stream_1 = require("level-read-stream");
+__exportStar(require("./interfaces"), exports);
 function setup(dbpath, filename = '', verbose = false, omitPartial = false) {
     return __awaiter(this, void 0, void 0, function* () {
-        const db = levelup_1.default(leveldown_1.default(dbpath));
+        const db = new classic_level_1.ClassicLevel(dbpath);
         try {
-            const opt = { asBuffer: false };
-            const [dictDate, version] = yield Promise.all([db.get('raw/dictDate', opt), db.get('raw/version', opt)]);
+            const [dictDate, version] = yield Promise.all([db.get('raw/dictDate'), db.get('raw/version')]);
             return { db, dictDate, version };
         }
         catch (_a) {
             // pass
         }
         if (!filename) {
+            yield db.close();
             throw new Error('database not found but cannot create it if no `filename` given');
         }
         let contents = '';
@@ -104,17 +113,19 @@ function drainStream(stream) {
 function searchBeginning(db, prefix, key = 'kana', limit) {
     return __awaiter(this, void 0, void 0, function* () {
         const gte = `indexes/${key}/${prefix}`;
-        return idsToWords(db, yield drainStream(db.createValueStream({ gte, lt: gte + '\uFE0F', valueAsBuffer: false, limit })));
+        const values = new level_read_stream_1.ValueStream(db, { gte, lt: gte + '\uFE0F', limit });
+        return idsToWords(db, yield drainStream(values));
     });
 }
 function searchAnywhere(db, text, key = 'kana', limit) {
     return __awaiter(this, void 0, void 0, function* () {
         const gte = `indexes/partial-${key}/${text}`;
-        return idsToWords(db, yield drainStream(db.createValueStream({ gte, lt: gte + '\uFE0F', valueAsBuffer: false, limit })));
+        const values = new level_read_stream_1.ValueStream(db, { gte, lt: gte + '\uFE0F', limit });
+        return idsToWords(db, yield drainStream(values));
     });
 }
 function idsToWords(db, idxs) {
-    return Promise.all(idxs.map(i => db.get(`raw/words/${i}`, { asBuffer: false }).then(x => JSON.parse(x))));
+    return Promise.all(idxs.map(i => db.get(`raw/words/${i}`).then((x) => JSON.parse(x))));
 }
 exports.idsToWords = idsToWords;
 function readingBeginning(db, prefix, limit = -1) {
@@ -143,13 +154,13 @@ function kanjiAnywhere(db, text, limit = -1) {
 exports.kanjiAnywhere = kanjiAnywhere;
 function getTags(db) {
     return __awaiter(this, void 0, void 0, function* () {
-        return db.get('raw/tags', { asBuffer: false }).then(x => JSON.parse(x));
+        return db.get('raw/tags').then((x) => JSON.parse(x));
     });
 }
 exports.getTags = getTags;
 function getField(db, key) {
     return __awaiter(this, void 0, void 0, function* () {
-        return db.get(`raw/${key}`, { asBuffer: false });
+        return db.get(`raw/${key}`);
     });
 }
 exports.getField = getField;
