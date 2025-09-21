7f72cec9e24d476d844c9b62982e46f165e37542
Ahmed Fasih <wuzzyview@gmail.com>
2025-09-20 21:16:01 -0700

    nuke old js files

--- interfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GlossType = void 0;
-var GlossType;
-(function (GlossType) {
-    GlossType["literal"] = "literal";
-    GlossType["figurative"] = "figurative";
-    GlossType["explanation"] = "explanation";
-})(GlossType = exports.GlossType || (exports.GlossType = {}));
